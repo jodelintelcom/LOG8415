d7559b7596b24b49a2027a843909e088d7afd982
Jodel <marcjodel.a@gmail.com>
2025-09-01 11:17:28 -0400

    update: network module

---
+++ terraform/modules/network/outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "The ID of our VPC"
+  value       = aws_vpc.this.id
+}
+
+output "subnet_ids" {
+  description = "The list of IDs of our public subnets"
+  value       = aws_subnet.public.*.id
+}
