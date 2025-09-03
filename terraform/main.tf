f50b84bca82de9f4b46bad50b6e13cddd2e58110
Jodel <marcjodel.a@gmail.com>
2025-09-02 21:02:39 -0400

    update: calling modules in root

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,18 @@ provider "aws" {
   region = "ca-central-1"
 }
 
+module "cluster1" {
+  source         = "./modules/cluster"
+  cluster_name   = "cluster1"
+  subnet_ids     = module.network.subnet_ids
+  instance_count = 5
+  instance_type  = "t2.micro"
+}
+
+module "cluster2" {
+  source         = "./modules/cluster"
+  cluster_name   = "cluster2"
+  subnet_ids     = module.network.subnet_ids
+  instance_count = 4
+  instance_type  = "t2.large"
+}
