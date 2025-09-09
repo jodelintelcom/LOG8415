9c37dd9175061e4737cbacb86300241248b7eda6
Martin Roch-Neirey <martin.roch-neirey@hefr.ch>
2025-09-09 19:31:49 -0400

    Add: cluster structure and ALBv1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+module "vpc" {
+  source     = "./modules/network"
+  vpc_cidr   = "10.0.0.0/16"
+  public_subnet_cidr = ["0.0.0.0/0"]
+}
+
 module "cluster1" {
   source          = "./modules/cluster"
   cluster_name    = "cluster1"
@@ -7,7 +13,19 @@ module "cluster1" {
   key_name = "ssh_key"
 }
 
+module "cluster2" {
+  source          = "./modules/cluster"
+  cluster_name    = "cluster2"
+  instances_count = 2
+  instance_type   = "t2.micro"
+  ami_id          = "ami-0360c520857e3138f"
+  key_name = "ssh_key"
+}
+
+/*
 module "alb" {
   source   = "./modules/alb"
   alb_name = "alb"
+  vpc_id   = module.vpc.vpc_id
 }
+*/
--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,9 +1,11 @@
 output "vpc_id" {
   description = "The ID of our VPC"
-  value       = aws_vpc.this.id
+  value       = aws_vpc.main.id
 }
 
+/*
 output "subnet_ids" {
   description = "The list of IDs of our public subnets"
   value       = aws_subnet.public[*].id
 }
+*/
